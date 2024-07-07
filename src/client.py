bc06a50c03a2ab3e956a3b041d55bc90252d8aa9
doward <nadegdaspp@gmail.com>
2024-07-07 18:06:58 +0300

    add encryption

--- src/client.py
+++ src/client.py
@@ -2,30 +2,90 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
+import encryption
 
+
+'''
+id message
+
+000 registration user
+001 get public keys
+002 swap partial keys
+003 message
+004 choose user
+005 leave user
+'''
+on_encrypt = False
+
+def send_server(server: socket, message: str, id = '003'):
+    server.send(bytes(id + ' ' + message, "utf8"))
+
+def set_keys(public_key1: int, public_key2: int) -> None:
+    encrypt.set_public_keys(public_key1, public_key2)
+
+def unable_encryption() -> None:
+    global id, on_encrypt
+    id = '003'
+    on_encrypt = True
 
 def receive():
+    global on_encrypt, encrypt
+
     while True:
         try:
-            msg = client_socket.recv(BUFSIZE).decode("utf8")
-            msg_list.insert(tkinter.END, msg)
+            id, *msg = client_socket.recv(BUFSIZE).decode("utf8").split()
+            print(id, msg)
+            
+            match id:
+                case '001': #get public keys and send partial_key
+                    msg_list.insert(tkinter.END, "gets public keys, please wait")
+                    public_key1 = int(msg[0])
+                    public_key2 = int(msg[1])
+                    set_keys(public_key1, public_key2)
+                    send_server(client_socket, '', '011')
+                case '011':
+                    send_server(client_socket, str(encrypt.create_partial_key()), '002')
+                case '002': 
+                    msg_list.insert(tkinter.END, "create full keys, please wait")
+                    partial_key = int(msg[0])
+                    encrypt.create_full_key(partial_key)
+                    msg_list.insert(tkinter.END, "done")
+                    unable_encryption()
+                case '003':
+                    if on_encrypt:
+                        msg = ''.join(msg)
+                        name, *msg = msg.split(':')
+                        msg = ''.join(msg)
+                        msg = name + ': ' + encrypt.decrypt_message(msg)
+                        
+                    msg_list.insert(tkinter.END, msg)
         except OSError:
             break
 
 
 def send(event=None):
+    global id, on_encrypt, encrypt
+    print(id)
     msg = my_msg.get()
     my_msg.set("")
-    client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
+
+    if msg == "(quit)":
+        id = '005'
+    if on_encrypt:
+        msg = encrypt.encrypt_message(''.join(msg))
+    send_server(client_socket, msg, id)
+
+    if msg == "(quit)":
         client_socket.close()
         top.quit()
 
 
 def on_closing(event=None):
-    my_msg.set("{quit}")
+    my_msg.set("(quit)")
     send()
 
+
+id = '000'
 top = tkinter.Tk()
 top.title("TkMessenger")
 
@@ -57,6 +117,8 @@ else:
 
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
+
+encrypt = encryption.Encryption()
 
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(ADDR)
--- src/server.py
+++ src/server.py
@@ -1,8 +1,8 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-
-clients = {}
-addresses = {}
+from encryption import create_public_keys
+clients = []
+friend = {}
 HOST = '0.0.0.0'
 PORT = 33000
 BUFSIZE = 1024
@@ -10,39 +10,71 @@ ADDR = (HOST, PORT)
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
+def send(client: socket, message: str, id = '003'):
+    client.send(bytes(id + ' ' + message, "utf8"))
+
+def start_encryption(client: socket):
+
+    public_key1, public_key2 = create_public_keys()
+    send(client, str(public_key1) + ' ' + str(public_key2), '001')
+    send(friend[client], str(public_key1) + ' ' + str(public_key2), '001')
+    
+
 def accept_connections():
     while True:
         client, client_addr = SERVER.accept()
         print('CONNECTED:', client_addr)
-        client.send(bytes("Write your name: ", "utf8"))
+        send(client, "Write your name: ")
 
-        addresses[client] = client_addr
+        
+        clients.append(client)
+        
         Thread(target=handle_client, args=(client,)).start()
 
-def handle_client(client):
-    name = client.recv(BUFSIZE).decode("utf8")
+def registration(client: socket, name: str):
+    send(client, f'Welcome {name}! Write (quit) to leave.')
     
-    welcome = f'Welcome {name}! Write (quit) to leave.'
-    client.send(bytes(welcome, "utf8"))
-    broadcast(bytes(f'{name} joined', "utf8"))
 
-    clients[client] = name
-
+    if len(clients) == 2:
+            friend[client] = clients[0]
+            friend[clients[0]] = client
+            send(friend[client], f'{name} is online', "003")
+            start_encryption(client)
+    
+cnt_ready = 0
+def handle_client(client: socket):
+    global cnt_ready
+    name = ''
+    
     while True:
-        msg = client.recv(BUFSIZE)
-        if msg != bytes("(quit)", "utf8"):
-            broadcast(msg, name + ': ')
-        else:
-            client.send(bytes("(quit)", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes(f"{name} leave.", "utf8"))
-            break
-
-
-def broadcast(message, prefix = ''):
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8") + message)
+        id, *msg = client.recv(BUFSIZE).decode("utf8").split()
+        msg = ' '.join(msg)
+        print(id, msg)
+        
+        match id:
+            case '000':
+                name = msg
+                registration(client, name)
+            case '011':
+                cnt_ready += 1
+                if cnt_ready == 2:
+                    send(client, '', '011')
+                    send(friend[client], '', '011')
+            case '002':
+                partial_key = msg
+                send(friend[client], partial_key, '002')
+            case '003':
+                send(friend[client], name + ':' + msg, '003')
+                send(client, name + ':' + msg, '003')
+            case '005':
+                client.send(bytes("(quit)", "utf8"))
+                client.close()
+                del clients[client]
+                
+                send(friend[client], name + 'leave', '003')
+                send(client, name + 'leave' '003')
+                break
+            
 
 if __name__ == "__main__":
     SERVER.listen(2)
